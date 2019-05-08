7a7ad04e23f0ef50f5dd7adc46f904f7f601bb5a
Nikolay Lapay <nl@wildtuna.pro>
2019-05-08 21:46:49 +0300

    Fix strict types in Exception

--- src/Exception/BoxBerryException.php
+++ src/Exception/BoxBerryException.php
@@ -2,8 +2,6 @@
 
 namespace WildTuna\BoxberrySdk\Exception;
 
-use Throwable;
-
 class BoxBerryException extends \Exception
 {
     /**
@@ -16,7 +14,7 @@ class BoxBerryException extends \Exception
      */
     private $raw_request  = null;
 
-    public function __construct($message = "", $code = 0, $raw_response = null, $raw_request = null, Throwable $previous = null)
+    public function __construct($message = "", $code = 0, $raw_response = null, $raw_request = null, $previous = null)
     {
         $this->raw_request = $raw_request;
         $this->raw_response = $raw_response;
@@ -26,7 +24,7 @@ class BoxBerryException extends \Exception
     /**
      * @return string
      */
-    public function getRawResponse(): string
+    public function getRawResponse()
     {
         return $this->raw_response;
     }
@@ -34,7 +32,7 @@ class BoxBerryException extends \Exception
     /**
      * @param string $raw_response
      */
-    public function setRawResponse(string $raw_response): void
+    public function setRawResponse($raw_response)
     {
         $this->raw_response = $raw_response;
     }
@@ -42,7 +40,7 @@ class BoxBerryException extends \Exception
     /**
      * @return string
      */
-    public function getRawRequest(): string
+    public function getRawRequest()
     {
         return $this->raw_request;
     }
@@ -50,7 +48,7 @@ class BoxBerryException extends \Exception
     /**
      * @param string $raw_request
      */
-    public function setRawRequest(string $raw_request): void
+    public function setRawRequest($raw_request)
     {
         $this->raw_request = $raw_request;
     }
